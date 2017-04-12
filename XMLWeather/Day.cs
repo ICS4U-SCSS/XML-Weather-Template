c14e314c56a6179d400b1ba2aa0b274b5c8ce9db
Mr. T. <tony.theodoropoulos@ed.amdsb.ca>
2017-04-12 10:34:40 -0400

    Included a Day Class

---
+++ XMLWeather/Day.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace XMLWeather
+{
+    public class Day
+    {
+        public string date, currentTemp, currentTime, condition, location, tempHigh, tempLow, 
+            windSpeed, windDirection, precipitation, visibility;
+
+        public Day()
+        {
+            date = currentTemp = currentTime = condition = location = tempHigh = tempLow
+                = windSpeed = windDirection = precipitation = visibility = "";
+        }
+    }
+}
