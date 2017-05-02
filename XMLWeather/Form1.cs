54b4869c1296eeb8049df5ffeec1ddcc6d2de46e
theodot <theodot@outlook.com>
2017-05-02 12:34:29 -0400

    Template reset

--- XMLWeather/Form1.cs
+++ XMLWeather/Form1.cs
@@ -37,74 +37,17 @@ namespace XMLWeather
             client.DownloadFile("http://api.openweathermap.org/data/2.5/weather?q=Stratford,CA&mode=xml&units=metric&appid=3f2e224b815c0ed45524322e145149f0", "WeatherData.xml");
             // mulit day forecast
             client.DownloadFile("http://api.openweathermap.org/data/2.5/forecast/daily?q=Stratford,CA&mode=xml&units=metric&cnt=7&appid=3f2e224b815c0ed45524322e145149f0", "WeatherData7Day.xml");
-       
         }
 
         private void ExtractCurrent()
         {
             XmlDocument doc = new XmlDocument();
             doc.Load("WeatherData.xml");
-
-            XmlNode parent;
-            parent = doc.DocumentElement;
-
-            foreach (XmlNode child in parent.ChildNodes)
-            {
-                if (child.Name == "city")
-                {
-                    d.location = child.Attributes["name"].Value;
-                }
-
-                if (child.Name == "temperature")
-                {
-                    d.tempHigh = child.Attributes["max"].Value;
-                    d.tempLow = child.Attributes["min"].Value;
-                    d.currentTemp = child.Attributes["value"].Value;
-                }
-
-            }
-
-            days.Add(d);
-
         }
               
         private void ExtractForecast()
         {
-            XmlDocument doc = new XmlDocument();
-            doc.Load("WeatherData7Day.xml");
 
-            XmlNode parent;
-            parent = doc.DocumentElement;
-
-            d = new Day();
-
-            foreach (XmlNode child in parent.ChildNodes)
-            {
-                if (child.Name == "forecast")
-                {
-                    foreach (XmlNode grandChild in child.ChildNodes)
-                    {
-                        foreach (XmlNode greatGrandChild in grandChild.ChildNodes)
-                        {
-                            d.date = grandChild.Attributes["day"].Value;
-
-                            if (greatGrandChild.Name == "temperature")
-                            {
-                                d.tempHigh = greatGrandChild.Attributes["max"].Value;
-                                d.tempLow = greatGrandChild.Attributes["min"].Value;
-                            }
-
-                            if (greatGrandChild.Name == "clouds")
-                            {
-                                d.condition = greatGrandChild.Attributes["value"].Value;
-                                days.Add(d);
-                                d = new Day();
-                            }
-                        }
-                    }
-                }
-            }
-            days.RemoveAt(1);
         }
     }
 }
